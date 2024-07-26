930f6dc2ce51be03640a117cc89645a5f97a4ef4
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-07-25 22:02:56 -0600

    fixed fight history data structure

--- django-react-app/backend/scripts/analysis.py
+++ django-react-app/backend/scripts/analysis.py
@@ -4,24 +4,24 @@ import ast
 def load_data(file_path):
     return pd.read_excel(file_path)
 
-def convert_to_dict(career_stats):
-    if isinstance(career_stats, str):
-        return ast.literal_eval(career_stats)
-    return career_stats
+def convert_to_dict(fighting_career):
+    if isinstance(fighting_career, str):
+        return ast.literal_eval(fighting_career)
+    return fighting_career
 
 def main():
     file_path = 'cleaned_fighter_data.xlsx'  # Update with your file path
     df = load_data(file_path)
-
+    
+    df.drop(columns=['Nickname'], inplace=True)
     # Convert Career Statistics column to dictionaries
     df['Career Statistics'] = df['Career Statistics'].apply(convert_to_dict)
-
+    df['Fight History'] = df['Fight History'].apply(convert_to_dict)
     # Access a specific value within the first row's Career Statistics dictionary
     first_row_career_stats = df['Career Statistics'][4]
-    print(first_row_career_stats)
-    
-    # Access a specific key's value
-    print(first_row_career_stats[''])  # Replace 'SLpM' with the key you want to access
+    # print(first_row_career_stats)
+    # print(df.head())
+    # print(first_row_career_stats[''])  
 
 if __name__ == '__main__':
     main()
