1c6f1975b22b85116889e44a6b22a19a97a122b1
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-07-25 22:23:16 -0600

    cleaned the dataset.

--- django-react-app/backend/scripts/analysis.py
+++ django-react-app/backend/scripts/analysis.py
@@ -4,10 +4,15 @@ import ast
 def load_data(file_path):
     return pd.read_excel(file_path)
 
-def convert_to_dict(fighting_career):
-    if isinstance(fighting_career, str):
-        return ast.literal_eval(fighting_career)
-    return fighting_career
+def convert_to_dict(x):
+    try:
+        # For 'Record' we need to strip "RECORD: " and then convert to dictionary
+        if isinstance(x, str) and x.startswith('RECORD: '):
+            return x.replace('RECORD: ', '').strip()
+        return ast.literal_eval(x)
+    except (ValueError, SyntaxError):
+        return x
+
 
 def main():
     file_path = 'cleaned_fighter_data.xlsx'  # Update with your file path
@@ -15,12 +20,13 @@ def main():
     
     df.drop(columns=['Nickname'], inplace=True)
     # Convert Career Statistics column to dictionaries
+    # df['First Name'] = df['First Name'].apply(convert_to_dict)
+    # df['Last Name'] = df['Last Name'].apply(convert_to_dict)
     df['Career Statistics'] = df['Career Statistics'].apply(convert_to_dict)
     df['Fight History'] = df['Fight History'].apply(convert_to_dict)
-    # Access a specific value within the first row's Career Statistics dictionary
-    first_row_career_stats = df['Career Statistics'][4]
-    # print(first_row_career_stats)
-    # print(df.head())
+    df['Record'] = df['Record'].apply(convert_to_dict)
+
+    print(df['First Name'][3])
     # print(first_row_career_stats[''])  
 
 if __name__ == '__main__':
